d1272215af96f5c8e85a6271861be8d414410cc1
tchi-sama <tchisama41@gmail.com>
2023-09-22 16:35:12 -0700

    making the menu as welll as the dailog of view code

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,84 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Send ,RocketIcon, Redo ,Undo,ArrowRight,Copy,Eye } from 'lucide-react';
+import { Send ,ArrowLeft,Save,Plus,RocketIcon, Redo ,Undo,ArrowRight,Copy,Eye } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-
+import { UserButton } from '@clerk/nextjs/app-beta';
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarTrigger } from '@/components/ui/menubar';
+import { Dialog, DialogClose, DialogTrigger } from '@radix-ui/react-dialog';
+import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 interface State {
   html: string;
 }
 
 export default function Home() {
-  const [htmlContent, setHtmlContent] = useState<string>('');
+  const [htmlContent, setHtmlContent] = useState<string>(`
+  <html>
+  <head> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> 
+  <style>
+  .ITEM-CARD {
+    background-color: #f2f2f2;
+    width: 300px;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  .ITEM-IMAGE {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 5px;
+  }
+  
+  .ITEM-NAME {
+    font-weight: bold;
+    margin-top: 10px;
+  }
+  
+  .ITEM-PRICE {
+    color: blue;
+    margin-top: 5px;
+  }
+  
+  .ITEM-DESCRIPTION {
+    margin-top: 5px;
+  }
+  
+  .ITEM-BUTTON {
+    background-color: #4caf50;
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 5px;
+    margin-top: 10px;
+  }
+  
+  .ITEM-BUTTON:hover {
+    background-color: #45a049;
+  }
+  </style>
+  </head>
+  <body>
+  
+  <div class="ITEM-CARD">
+    <img src="https://th.bing.com/th/id/R.77ca77d32b8b8e97122ff20b2fff9658?rik=gFvl%2bFaxCCSU9A&riu=http%3a%2f%2fcdn.wallpapersafari.com%2f24%2f66%2fjVJNKu.png&ehk=BeYPDSonYIebD%2fPBR8lb2uwto%2fWI46j07m6ceP5IG2c%3d&risl=&pid=ImgRaw&r=0" class="ITEM-IMAGE" alt="Item Image">
+    <h3 class="ITEM-NAME">Item Name</h3>
+    <p class="ITEM-PRICE">$10</p>
+    <p class="ITEM-DESCRIPTION">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <button class="ITEM-BUTTON">Add to Cart <i class="fas fa-shopping-cart"></i></button>
+  </div>
+  
+  </body>
+  </html> 
+  `);
   const [message, setMessage] = useState<string>('');
   const [loading, setLoading] = useState(false);
   const [history, setHistory] = useState<State[]>([]);
@@ -62,10 +129,89 @@ export default function Home() {
 
   return (
     <div className='min-h-screen  gap-2 bg-white w-full '>
-      <div className='container border top-2 shadow left-[50%] translate-x-[-50%] fixed p-4 bg-white rounded-xl flex justify-between'>
-        <h1>Name</h1>
+      <div className='container items-center border top-2 shadow left-[50%] translate-x-[-50%] fixed p-2 bg-white rounded-xl flex justify-between'>
+        <div className='flex items-center gap-4 px-1'>
+        <UserButton/>
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>File</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem className="flex gap-2"><ArrowLeft size={16}/>Go back</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem className="flex gap-2">
+                <Plus size={16}/>
+                New WorkSpace 
+              </MenubarItem>
+              <MenubarItem className="flex gap-2"><Save size={16}/>Save</MenubarItem>
+              <MenubarItem className="flex gap-2"><Save size={16}/>Save As</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+
+          <MenubarMenu>
+            <MenubarTrigger>Edit</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem className="flex gap-2"><Undo size={16}/>Undo</MenubarItem>
+              <MenubarItem className="flex gap-2"><Redo size={16}/>Redo</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+
+          <MenubarMenu>
+            <MenubarTrigger>Control</MenubarTrigger>
+            <MenubarContent>
+              <MenubarItem className="flex gap-2"><Copy size={16}/>Copy code</MenubarItem>
+              <MenubarItem className="flex gap-2"><Eye size={16}/>View code</MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
+
+        </div>
         <div className='flex gap-2'>
-          <Button className='flex gap-2'>Veiw code <Eye size={18}></Eye></Button>
+          <Dialog >
+            <DialogTrigger className='flex gap-2' asChild>
+                <Button >Veiw code <Eye size={18}></Eye></Button>
+            </DialogTrigger>
+            <DialogContent className='w-screen' >
+              <DialogHeader>
+                <DialogTitle>Tomplate code</DialogTitle>
+              </DialogHeader>
+              <DialogDescription className='flex gap-2'>
+                <Select>
+                  <SelectTrigger defaultValue={"html"} className="flex-1">
+                    <SelectValue placeholder="Framework" />
+                  </SelectTrigger>
+                  <SelectContent >
+                    <SelectItem value="html">Html</SelectItem>
+                    <SelectItem value="reactjs">Reactjs</SelectItem>
+                    <SelectItem value="vuejs">Vuejs</SelectItem>
+                  </SelectContent>
+                </Select>
+                <Select>
+                  <SelectTrigger className="flex-1">
+                    <SelectValue placeholder="Styling" />
+                  </SelectTrigger>
+                  <SelectContent defaultValue={"css"}>
+                    <SelectItem value="css">Css</SelectItem>
+                    <SelectItem value="tailwindcss">Tailwind css</SelectItem>
+                  </SelectContent>
+                </Select>
+                <Button>Recode</Button>
+              </DialogDescription>
+              <DialogDescription className='w-fit'>
+                <pre contentEditable className=' w-full outline-none max-w-[465px] overflow-auto max-h-[60vh]'>
+                {htmlContent?htmlContent:"no code to preview" }
+                </pre>
+              </DialogDescription>
+              <DialogFooter >
+
+                <Button className='flex gap-2' >Copy <Copy size={14}></Copy></Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+
+
+
+
+
           <Button className='flex gap-2' variant={"outline"}>Copy<Copy size={18}></Copy></Button>
         </div>
       </div>
@@ -99,8 +245,8 @@ export default function Home() {
                   }
         </div>
       <div className=' p-2 shadow-md border fixed bottom-4 z-50 left-[50%] translate-x-[-50%] max-w-4xl flex gap-2 w-full mx-auto bg-white rounded-lg'>
-        <Button onClick={undo} variant={"outline"}><Undo/></Button>
-        <Button onClick={redo} variant={"outline"}><Redo/></Button>
+        <Button onClick={undo}  variant={"outline"}><Undo size={18}/></Button>
+        <Button onClick={redo} variant={"outline"}><Redo size={18}/></Button>
         <form onSubmit={Genirate} className='flex-1 flex gap-2'>
           <Input value={message} onInput={(e)=>setMessage((e.target as any).value)}/>
           <Button type='submit' className='flex gap-2'>{loading?"Loading":<>Send<Send size={20}/></>}</Button>
